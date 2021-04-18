6ccfe4e2d35e8a81d2777abd6ae340b3a5d974d3
Bruno Gomes <brunogomesdeveloper@gmail.com>
2021-04-18 17:12:29 -0300

    ajuste heroku

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 require('dotenv').config()
 
-app.listen(3333, () => {
+app.listen(process.env.PORT, () => {
     console.log(`API rodando na porta ${process.env.PORT}`);
 });
 
